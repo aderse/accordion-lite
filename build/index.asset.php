0b8ecb9ed90f6e5cb4b77018524508b0793b1e5e
Andrew Derse <aderse@back2back.org>
2024-01-10 12:08:03 -0600

    Setting version to 1.0.6

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '95f90d960d35691b5d13');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'cd57e3936b8d3a54578f');
